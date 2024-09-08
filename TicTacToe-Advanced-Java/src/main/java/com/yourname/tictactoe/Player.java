5bfb907291921b27eaf8dce007fd6a25c14296db
O-DevOps-1 <ofattahserir@gmail.com>
2024-09-08 23:15:35 +0200

    Update Player.java

---
+++ TicTacToe-Advanced-Java/src/main/java/com/yourname/tictactoe/Player.java
@@ -0,0 +1,18 @@
+package com.yourname.tictactoe;
+
+public class Player {
+    private final String name;
+    private final String symbol;
+
+    public Player(String name, String symbol) {
+        this.name = name;
+        this.symbol = symbol;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }}
