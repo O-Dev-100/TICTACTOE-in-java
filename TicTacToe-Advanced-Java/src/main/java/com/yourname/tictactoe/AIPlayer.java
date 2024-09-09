19645ec24830d205abd3f3a9501b5243984e3d37
O-DevOps-1 <ofattahserir@gmail.com>
2024-09-09 11:21:48 +0200

    Update AIPlayer.java
    Used an API to avoid code's redundancy and enhance its quality

--- TicTacToe-Advanced-Java/src/main/java/com/yourname/tictactoe/AIPlayer.java
+++ TicTacToe-Advanced-Java/src/main/java/com/yourname/tictactoe/AIPlayer.java
@@ -1,58 +1,96 @@
-package com.yourname.tictactoe;
+package com.odev1.tictactoe;
+
+import fr.avianey.minimax4j.IA;
+import fr.avianey.minimax4j.Move;
+import fr.avianey.minimax4j.impl.Minimax;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AIPlayer extends Player {
+
     public AIPlayer(String name, String symbol) {
         super(name, symbol);
     }
 
-    public void makeMove(Game game) {
-        int[] bestMove = findBestMove(game);
-        game.makeMove(bestMove[0], bestMove[1], this.getSymbol());
+    public int[] makeMove(String[][] board) {
+        TicTacToeIA ia = new TicTacToeIA(board, this.symbol);
+        Move bestMove = ia.getBestMove();
+        return new int[] { bestMove.getX(), bestMove.getY() };
     }
 
-    private int[] findBestMove(Game game) {
-        int bestVal = Integer.MIN_VALUE;
-        int[] bestMove = {-1, -1};
+    private class TicTacToeIA extends Minimax {
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (game.isCellEmpty(i, j)) {
-                    game.makeMove(i, j, this.getSymbol());
-                    int moveVal = minimax(game, 0, false);
-                    game.undoMove(i, j);
+        private String[][] board;
+        private String symbol;
 
-                    if (moveVal > bestVal) {
-                        bestMove[0] = i;
-                        bestMove[1] = j;
-                        bestVal = moveVal;
-                    }}}}
-      
-        return bestMove;
-    }
+        public TicTacToeIA(String[][] board, String symbol) {
+            this.board = board;
+            this.symbol = symbol;
+        }
 
-    private int minimax(Game game, int depth, boolean isMax) {
-        int score = game.evaluate();
+        @Override
+        public boolean isOver() {
+            return checkWin(board) || checkDraw(board);
+        }
 
-        if (score == 10) return score - depth;
-        if (score == -10) return score + depth;
-        if (!game.isMovesLeft()) return 0;
+        @Override
+        public int evaluate() {
+            return evaluateBoard(board);
+        }
 
-        if (isMax) {
-            int best = Integer.MIN_VALUE;
+        @Override
+        public Iterable<Move> getPossibleMoves() {
+            List<Move> moves = new ArrayList<>();
             for (int i = 0; i < 3; i++) {
                 for (int j = 0; j < 3; j++) {
-                    if (game.isCellEmpty(i, j)) {
-                        game.makeMove(i, j, this.getSymbol());
-                        best = Math.max(best, minimax(game, depth + 1, false));
-                        game.undoMove(i, j); }}}
-            return best;
-        } else {
-            int best = Integer.MAX_VALUE;
+                    if (board[i][j].equals("")) {
+                        moves.add(new Move(i, j));
+                    }}}
+            
+            return moves;
+        }
+
+        @Override
+        public void makeMove(Move move) {
+            board[move.getX()][move.getY()] = symbol;
+        }
+
+        @Override
+        public void unmakeMove(Move move) {
+            board[move.getX()][move.getY()] = "";
+        }
+
+        private boolean checkWin(String[][] board) {
+            for (int i = 0; i < 3; i++) {
+                if (board[i][0].equals(board[i][1]) && board[i][1].equals(board[i][2]) && !board[i][0].equals("")) {
+                    return true;
+                }
+                if (board[0][i].equals(board[1][i]) && board[1][i].equals(board[2][i]) && !board[0][i].equals("")) {
+                    return true;
+                }
+            }
+            if (board[0][0].equals(board[1][1]) && board[1][1].equals(board[2][2]) && !board[0][0].equals("")) {
+                return true;
+            }
+            if (board[0][2].equals(board[1][1]) && board[1][1].equals(board[2][0]) && !board[0][2].equals("")) {
+                return true;
+            }
+            return false;
+        }
+
+        private boolean checkDraw(String[][] board) {
             for (int i = 0; i < 3; i++) {
                 for (int j = 0; j < 3; j++) {
-                    if (game.isCellEmpty(i, j)) {
-                        game.makeMove(i, j, game.getOpponentSymbol(this.getSymbol()));
-                        best = Math.min(best, minimax(game, depth + 1, true));
-                        game.undoMove(i, j); }}}
-            return best;
+                    if (board[i][j].equals("")) {
+                        return false;
+                    }}}
+            
+            return true;
+        }
+
+        private int evaluateBoard(String[][] board) {
+            if (checkWin(board)) {
+                return symbol.equals("X") ? 10 : -10;
+            }
+            return 0;
         }}}
